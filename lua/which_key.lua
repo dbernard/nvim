4f8581d6fb906ce978c7684640cb9273c736fd7b
Dylan Bernard <dylan.bernard@rover.com>
2021-08-18 15:19:12 -0700

    more floaterm

--- lua/which_key.lua
+++ lua/which_key.lua
@@ -5,6 +5,7 @@ wk.register({
     name = "+terminal",
     q = { "<cmd>FloatermNew --wintype=popup --height=10<cr>", "Quick Terminal" }, -- create a new popup footer terminal
     t = { "<cmd>FloatermToggle<cr>", "Toggle Terminal" }, -- toggle a terminal
-    p = { "<cmd>FloatermNew python", "Python Terminal" }, -- open a python shell
+    p = { "<cmd>FloatermNew python<cr>", "Python Terminal" }, -- open a python shell
+    f = { "<cmd>FloatermNew fzf<cr>", "fzf Terminal" }, -- open an fzf session
   },
 }, { prefix = "<leader>" })

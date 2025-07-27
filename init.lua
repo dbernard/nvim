5e3eaf92e9d88fcdb7a36b48ea4d39c485dcd8d6
Dylan Bernard <dylan.bernard@rover.com>
2025-07-27 13:31:45 -0700

    Use codespace system python dynamically

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
+-- Use system Python 3 for Neovim
+-- This is a hack for my current codespace environment setup. Codespaces pull
+-- in my Neovim config before virtualenv configuration, so pynvim gets installed
+-- to the system Python 3.
+local system_python = '/usr/local/python/current/bin/python3'
+if vim.fn.executable(system_python) == 1 then
+  vim.g.python3_host_prog = system_python
+end
+
 -- Fetch plugins with Packer
 require('plugins')
 
